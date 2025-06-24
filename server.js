f31d230d0b380a3e46c90273db15cf176c38b8ce
Yash Kumar Jha <115889463+Yash-j7@users.noreply.github.com>
2025-06-24 22:55:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ connectDb();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://urbanknit-f.onrender.com",
-    credentials: true,
-  })
-)
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
