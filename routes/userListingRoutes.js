04eee2e0bcc6f163d5464a2fc011c379b6890d01
Yash Kumar Jha <115889463+Yash-j7@users.noreply.github.com>
2025-08-23 12:45:44 +0530

    Update userListingRoutes.js

--- routes/userListingRoutes.js
+++ routes/userListingRoutes.js
@@ -10,7 +10,7 @@ import {
   getUserListings,
 } from "../controller/userListingController.js";
 import { requireSignIn } from "../middleware/authMiddleware.js";
-
+import formidable from "express-formidable";
 const router = express.Router();
 
 // Public routes
@@ -20,8 +20,12 @@ router.get("/photo/:listingId", getListingPhoto);
 router.get("/user/:userId", getUserListings);
 
 // Protected routes
-router.post("/", requireSignIn, createUserListing);
-router.put("/:slug", requireSignIn, updateListing);
+router.post(
+  "/",
+  requireSignIn,
+  formidable({ multiples: false }), // only here
+  createUserListing
+);router.put("/:slug", requireSignIn, updateListing);
 router.delete("/:slug", requireSignIn, deleteListing);
 router.put("/:slug/sold", requireSignIn, markAsSold);
 
